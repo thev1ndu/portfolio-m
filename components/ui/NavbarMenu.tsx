4cc4d9ee09c9a89de55621d5c7c2214b240f765a
ThevinduW <120010745+thev1ndu@users.noreply.github.com>
2025-01-27 19:49:17 +0530

    Update NavbarMenu.tsx

--- components/ui/NavbarMenu.tsx
+++ components/ui/NavbarMenu.tsx
@@ -1,5 +1,5 @@
 "use client";
-import React from "react";
+import React, { ReactNode } from "react";
 import { motion } from "framer-motion";
 import Link from "next/link";
 import Image from "next/image";
@@ -12,7 +12,7 @@ export const MenuItem = ({
   item: string;
 }) => {
   return (
-    <div onMouseEnter={() => setActive(item)} className="relative ">
+    <div onMouseEnter={() => setActive(item)} className="relative">
       <motion.p
         transition={{ duration: 0.3 }}
         className="cursor-pointer text-sm pl-2 pr-2 text-white hover:opacity-[0.9]"
@@ -25,16 +25,17 @@ export const MenuItem = ({
 
 export const Menu = ({
   setActive,
+  children, // Add children here
 }: {
   setActive: (item: string | null) => void;
+  children: ReactNode; // Define the type for children
 }) => {
   return (
     <nav
       onMouseLeave={() => setActive(null)} // resets the state
       className="relative rounded-full border border-transparent dark:bg-black dark:border-white/[0.2] bg-black shadow-input flex justify-center space-x-4 px-8 py-6 mx-auto w-[90%] sm:w-[80%] md:w-[400px] lg:w-[500px]"
-
     >
-      {children}
+      {children} {/* Render children */}
     </nav>
   );
 };
@@ -71,12 +72,19 @@ export const ProductItem = ({
   );
 };
 
-export const HoveredLink = ({ children, ...rest }: any) => {
+export const HoveredLink = ({
+  children,
+  ...rest
+}: {
+  children: ReactNode; // Type for children
+  [key: string]: any; // Type for the rest of the props
+}) => {
   return (
     <Link
       {...rest}
-      className="text-neutral-700 dark:text-neutral-200 hover:text-black "
+      className="text-neutral-700 dark:text-neutral-200 hover:text-black"
     >
+      {children} {/* Render children */}
     </Link>
   );
 };
