5584fbd53f110875b4fdc1d823647d82a46b2bdb
ThevinduW <120010745+thev1ndu@users.noreply.github.com>
2025-01-27 19:51:19 +0530

    Update TextEffectA.tsx

--- components/ui/TextEffectA.tsx
+++ components/ui/TextEffectA.tsx
@@ -15,7 +15,7 @@ export const TextGenerateEffect2 = ({
   duration?: number;
 }) => {
   const [scope, animate] = useAnimate();
-  let wordsArray = words.split(" ");
+  const wordsArray = words.split(" ");
   useEffect(() => {
     animate(
       "span",
